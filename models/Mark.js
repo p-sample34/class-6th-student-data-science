ff7c44c2fde0595211ec33912635d1b7b4cab5ae
p-sample34 <psample34@gmail.com>
2025-11-03 22:35:08 +0530

    Update Mark.js

--- models/Mark.js
+++ models/Mark.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const markSchema = new mongoose.Schema({
   name: String,
   roll: String,
-  subject: String,
-  marks: Number,
+  totalMarks: Number,
+  obtainedMarks: Number,
   month: String,
   date: { type: Date, default: Date.now }
 });
